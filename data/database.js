27c861560bf662e39529277f16fdf245b079604c
vikas9598 <vks984297@gmail.com>
2024-01-25 02:03:17 +0530

    update

--- data/database.js
+++ data/database.js
@@ -3,6 +3,6 @@ import mongoose from "mongoose";
 
 export const connectDB = () => {
     mongoose.connect(process.env.MONGO_URI, { dbName: "todoserver", })
-        .then(() => console.log("DB connected...!!"))
+        .then((c) => console.log(`database connected on ${c.connection.host}`))
         .catch((e) => console.log(e));
 }
